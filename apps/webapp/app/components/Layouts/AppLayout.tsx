c88eb2a08b8199467f0e4c5e2deb8c0cf442bb42
hetpatel@fynd-external.com <het.patel@devxconsultancy.com>
2025-06-21 16:07:06 +0530

    refactor: remove GuestSessionSync component and related API for guest session syncing

--- apps/webapp/app/components/Layouts/AppLayout.tsx
+++ apps/webapp/app/components/Layouts/AppLayout.tsx
@@ -1,19 +1,15 @@
-import { Outlet, useLoaderData } from "react-router";
+import { Outlet } from "react-router";
 import AppSidebar from "./App/AppSidebar";
 import { SidebarProvider } from "@justchat/ui/components/sidebar";
 import AppHeader from "./App/AppHeader";
 
 export default function AppLayout() {
-  const { user } = useLoaderData() as { user?: any };
-
   return (
     <SidebarProvider>
       <AppSidebar />
       <main className="grid grid-rows-[auto_1fr] w-full h-screen">
         <AppHeader />
-        <div className="relative">
-          <Outlet />
-        </div>
+        <Outlet />
       </main>
     </SidebarProvider>
   );
